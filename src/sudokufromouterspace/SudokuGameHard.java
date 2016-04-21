5fecdcc7a64bf64166ba0287f6069847714de6db
jean182 <jean-marco-10@hotmail.com>
2016-04-21 11:36:34 -0600

    Sudoku GUI

--- src/sudokufromouterspace/SudokuGameHard.java
+++ src/sudokufromouterspace/SudokuGameHard.java
@@ -470,7 +470,24 @@ public final class SudokuGameHard extends JFrame implements ActionListener, KeyL
     }
 
     public void loadGame(File fileName) {
+        for (int r = 0; r < sudokuMatrix.length; r++) {
 
+            for (int c = 0; c < sudokuMatrix[0].length; c++) {
+                labelPanel = (JLabel) sudokuMatrix[r][c].getComponent(0);
+                labelPanel.setText("");
+                sudokuMatrix[r][c].setEnabled(true);
+                if ((r / 3 + c / 3) % 2 == 0) {
+
+                    sudokuMatrix[r][c].setBackground(Color.getColor(nm, red));
+
+                } else {
+
+                    sudokuMatrix[r][c].setBackground(Color.white);
+                }
+            }
+
+        }
+        fillMatrixWithBasicSudoku();
         // This will reference one line at a time
         String line = null;
 
